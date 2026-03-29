d4425bb1a3a939ec26ce2d0173f04f7df2f4daa5
zaidprd <160503080+zaidprd@users.noreply.github.com>
2026-03-29 14:51:00 +0700

    Fix config error

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,20 +2,32 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sanity from '@sanity/astro';
-
 import react from '@astrojs/react';
+import cloudflare from '@astrojs/cloudflare';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  // 1. Tambahkan Sanity ke dalam Integrations
-  integrations: [sanity({
-    projectId: '0ukg7bxy', // Ganti dengan Project ID dari dashboard Sanity/file .env
-    dataset: 'production',
-    apiVersion: '2026-03-28', // Gunakan tanggal hari ini
-    useCdn: false, // Set false agar data selalu fresh (bagus untuk development)
-    studioUrl: '/admin', // Agar kamu bisa akses CMS di localhost:4321/admin
-  }), react()],
+  // 1. Masukkan domain kamu di sini buat SEO & Sitemap
+  site: 'https://zaidly.com', 
+
+  // 2. Integrations
+  integrations: [
+    sanity({
+      projectId: '0ukg7bxy', 
+      dataset: 'production',
+      apiVersion: '2026-03-28', 
+      useCdn: true, // Pakai true di lokal biar enteng (nggak berat)
+    }), 
+    react(), 
+    sitemap()
+  ],
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  // 3. Output 'server' wajib untuk Cloudflare Pages SSR
+  output: 'server',
+  adapter: cloudflare()
 });
