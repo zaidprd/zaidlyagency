ec28e34085eec78ad42c76e2f52a0a1753650cb0
zaidprd <160503080+zaidprd@users.noreply.github.com>
2026-03-29 18:02:48 +0700

    Refactor astro.config.mjs for static output and image service
    Updated Astro configuration to set image service and removed unnecessary comments.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,17 +6,18 @@ import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  // 1. Domain agensi lo
   site: 'https://zaidly.com',
+  output: 'static', 
 
-  // 2. PINDAH KE STATIC (Biar Cloudflare tinggal baca HTML)
-  output: 'static',
-
-  // 3. HAPUS ADAPTER CLOUDFLARE (Gak perlu import/panggil cloudflare lagi)
+  // INI OBATNYA: Paksa Astro buat GAK PAKAI Sharp
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/passthrough',
+    },
+  },
 
   integrations: [
     sanity({
-      // Tarik dari environment variables Cloudflare lo
       projectId: process.env.PUBLIC_SANITY_PROJECT_ID,
       dataset: process.env.PUBLIC_SANITY_DATASET,
       apiVersion: '2023-10-01',
@@ -29,7 +30,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
     ssr: {
-      // Jaga-jaga buat library node yang dipake Sanity
       external: ['node:events']
     }
   }
